77d9538514f1183507db6dc870c91c12d7c2d7c8
Vineet Mor <133258661+Vineet-Mor@users.noreply.github.com>
2024-06-24 17:08:01 -0600

    Create main.py

---
+++ cloud_function/main.py
@@ -0,0 +1,14 @@
+import json
+from google.cloud import pubsub_v1
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path('quixotic-treat-419302', 'traffic-data')
+
+def publish_traffic_data(request):
+    request_json = request.get_json()
+    if request_json and 'speed' in request_json:
+        data = json.dumps(request_json).encode('utf-8')
+        future = publisher.publish(topic_path, data)
+        return 'Message published.'
+    else:
+        return 'Invalid request.', 400
